4937cc869adf9262cc3e366c7c2c699eb6561604
BomeMyatKyaw <bomekyaw13@gmail.com>
2025-12-23 15:07:04 +0630

    Changes to be committed: 	new file:   app/Models/PriceChange.php 	new file:   database/migrations/2025_12_23_143206_create_price_changes_table.php 	new file:   database/migrations/2025_12_23_143244_price_chages_products_table.php

---
+++ database/migrations/2025_12_23_143206_create_price_changes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('price_changes', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->unsignedBigInteger('status_id')->default(1);
+            $table->dateTime('void_at')->nullable();
+            $table->unsignedBigInteger('void_by')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('price_changes');
+    }
+};
